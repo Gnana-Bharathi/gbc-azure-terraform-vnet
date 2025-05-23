29eee1485d386caa7475b6df175cba618eabfd3b
Gnana-Bharathi <gbharathi.work@gmail.com>
2025-05-23 13:40:48 -0600

    add outputs for ID, Location, Manged By details

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "resource_group_id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.rg.location
+}
+
+output "resource_group_managed_by" {
+  value = azurerm_resource_group.rg.managed_by
+}
