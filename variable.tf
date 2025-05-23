afaf6a481dd0b32be73f4e209d9664e6fb6faecf
Gnana-Bharathi <gbharathi.work@gmail.com>
2025-05-23 14:07:10 -0600

    add condition for tags to mandate required tags

--- variable.tf
+++ variable.tf
@@ -8,4 +8,15 @@ variable "location" {
 
 variable "tags" {
   type = map(string)
+
+  validation {
+    condition = alltrue([
+        contains(keys(var.tags), "PrimaryOwner"),
+        contains(keys(var.tags), "SecondaryOwner"),
+    ])
+    error_message = join("\n",[
+        contains(keys(var.tags), "Primary Owner") ? "" : "tag PrimaryOwner is required",
+        contains(keys(var.tags), "Primary Owner") ? "" : "tag SecondaryOwner is required"
+    ])
+  }
 }
